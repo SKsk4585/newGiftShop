a539b28a3ff8bc6b0771ab2b1944b6403b6c0fe0
SKsk4585 <5864585sari@gmail.com>
2023-05-06 22:41:27 +0300

    getGiftByAudienceTypes & add gift

--- backend/src/5-logic/giftShopLogic.ts
+++ backend/src/5-logic/giftShopLogic.ts
@@ -1,5 +1,7 @@
+import { OkPacket } from "mysql"
 import dal from "../2-utils/dal"
 import AudienceModel from "../4-models/audienceModel"
+import GiftModel from "../4-models/giftsModel"
 
 
 
@@ -10,7 +12,33 @@ async function getAllAudience(): Promise<AudienceModel[]>{
     return audience
 }
 
+async function getGiftByAudienceTypes(audienceId:number): Promise<GiftModel[]>{
+   const sql = `SELECT G.*, A.audienceType
+                 FROM gifts AS G JOIN audience AS A
+                 ON G.audienceId = A.audienceId
+                 WHERE G.audienceId = ${audienceId}`;
+   const gifts = await dal.execute(sql)
+    return gifts
+
+}
+
+async function addGift(gifts:GiftModel): Promise<GiftModel>{
+    const sql = `INSERT INTO gifts 
+                VALUES(DEFAULT,
+                    '${gifts.audienceId}'
+                    '${gifts.giftName}'
+                    '${gifts.Description}'
+                    '${gifts.price}'
+                    '${gifts.discount}'
+                    )`
+        const info:OkPacket = await dal.execute(sql)
+        gifts.giftId = info.insertId
+        return gifts
+}
+
 
 export default {
-    getAllAudience
+    getAllAudience,
+    getGiftByAudienceTypes,
+    addGift
 }
--- backend/src/6-controllers/giftShopController.ts
+++ backend/src/6-controllers/giftShopController.ts
@@ -1,17 +1,43 @@
 import express, { NextFunction, Request, Response } from "express"
 import giftShopLogic from "../5-logic/giftShopLogic"
+import GiftModel from "../4-models/giftsModel"
 
 const router = express.Router()
 
 //get all audience
 router.get("/audience",async (request:Request,respons:Response,next:NextFunction)=>{
     try {
-        const audience = await giftShopLogic.getAllAudience
+        const audience = await giftShopLogic.getAllAudience()
         respons.json(audience)
     } 
     catch (error) {
         next(error)        
     }
 })
+
+//get all gifts by audiencea type
+router.get("/gifts-by-audience-id/:audienceId",async (request:Request,respons:Response,next:NextFunction)=>{
+    try {
+        const audienceId = +request.params.audienceId;
+        const gifts = await giftShopLogic.getGiftByAudienceTypes(audienceId)
+        respons.json(gifts)
+    } 
+    catch (error) {
+        next(error)        
+    }
+})
+
+//add gift
+router.post("/gift",async (request:Request,response:Response,next:NextFunction)=>{
+    try {
+        const gift = new GiftModel(request.body)
+        const addedGift = await giftShopLogic.addGift(gift)
+        response.status(201).json(addedGift)
+    } 
+    catch (error) {
+        next(error) 
+       }
+               
+    })
 
 export default router
